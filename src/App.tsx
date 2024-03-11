837d95d47760e748759e80cc85565f9ec7557034
nahuelleiva <nahuel.nleiva@gmail.com>
2024-03-11 19:35:43 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import './App.css';
+
+type Country = {
+  id: number,
+  name: string
+};
+
+const countries = [
+  {
+    id: 1,
+    name: 'India'
+  },
+  {
+    id: 2,
+    name: 'USA'
+  },
+  {
+    id: 3,
+    name: 'India'
+  }
+] satisfies Country[];
+
+function App() {
+  const [selectedCountries, setSelectedCountries] = useState<Country[]>([]);
+  const areAllCountriesSelected = selectedCountries.length === countries.length;
+
+  return (
+    <>
+      <label>
+        <input 
+          type="checkbox" 
+          checked={areAllCountriesSelected} 
+          onChange={() => {
+            setSelectedCountries(areAllCountriesSelected ? [] : countries);
+          }}
+        />
+        Select All
+      </label>
+      <ul>
+        {
+          countries.map((country) => {
+            const isSelected = selectedCountries.find(({id})=> country.id === id) !== undefined;
+
+            return (
+              <li key={country.id}>
+                <label>
+                  <input 
+                    type="checkbox" 
+                    checked={isSelected}
+                    onChange={() => {
+                      if(isSelected) {
+                        setSelectedCountries(selectedCountries.filter(({id}) => country.id !== id));
+                      } else {
+                        setSelectedCountries([...selectedCountries, country]);
+                      }
+                    }}
+                  /> {country.name}
+                </label>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </>
+  )
+}
+
+export default App
